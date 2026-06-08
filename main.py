7269a77696ca910bedd5b1227d95a6cce5941c0b
RamiroGagoo <rgago@udesa.edu.ar>
2026-06-08 17:32:56 -0300

    Update main.py
    Actualize el main para probar la creacion del mapa general de CABA

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 #main
+from src.carga_datos_pandas import carga_datos
+from graficos.Grafico_general import crear_mapa
+
 ruta = "datos/airbnb.csv"
+
+df = carga_datos(ruta)
+
+if df is not None:
+
+    df_mapa = df.sample(100)
+
+    mapa = crear_mapa(df_mapa)
+    mapa.save("mapa_alojamientos.html")
+
+    print("Mapa generado correctamente.")
