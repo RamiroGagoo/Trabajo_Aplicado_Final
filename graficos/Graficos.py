7c83175b25c91554dae22ba3a79ceed2aa26d6bf
RamiroGagoo <rgago@udesa.edu.ar>
2026-06-08 17:23:49 -0300

    Update Graficos.py
    agregue la funcion que agrega alojamientos al mapa general de CABA

--- graficos/Graficos.py
+++ graficos/Graficos.py
@@ -1,4 +1,21 @@
 #graficos 
-"""
-Scatter graph de datos generales de CABA
-"""
+import folium
+
+def crear_mapa(df):
+    """
+    Descripción: Crea un mapa con los alojamientos.
+    Parámetros: df (DataFrame)
+    Retorno: mapa (folium)
+    """
+
+    mapa = folium.Map(location=[-34.60, -58.44], zoom_start=12)
+
+    for i, fila in df.iterrows():
+        lat = fila["latitude"]
+        lon = fila["longitude"]
+
+        folium.Marker(
+            location=[lat, lon]
+        ).add_to(mapa)
+
+    return mapa
